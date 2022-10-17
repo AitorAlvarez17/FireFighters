1331c243d905ab78c1e73020029d14dc42ce422a
AitorAlvarez17 <43441256+AitorAlvarez17@users.noreply.github.com>
2022-10-17 10:34:34 +0200

    client

--- Assets/Script/ClientScript.cs
+++ Assets/Script/ClientScript.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Net;
 using System.Threading;
 using UnityEngine;
 
-public class Socketstest : MonoBehaviour
+public class ClientScript : MonoBehaviour
 {
+    string IpEndAdress;
+    int port = 88;
+
+    //adress where we are going to connect
+    IPEndPoint ipep;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ipep = new IPEndPoint(IPAddress.Any, port);
     }
 
     // Update is called once per frame
