f368dd7e5e7b4d9e35a4739ab2f6f67bfb04f7be
andres <50046952+andreusama@users.noreply.github.com>
2023-01-03 15:24:52 +0100

    little fix

--- Assets/Script/WorldController.cs
+++ Assets/Script/WorldController.cs
@@ -162,8 +162,7 @@ public class WorldController : MonoBehaviour
             if (index.Item1 == _key)
             {
                 CreatePlayer(_key, true);
-                this.gameObject.GetComponent<PlayerMovement>().predicter = worldDolls[_key].movementPredicter.gameObject;
-                this.gameObject.GetComponent<PlayerMovement>().player = worldDolls[_key].lumberjack.gameObject;
+                this.gameObject.GetComponent<PlayerMovement>().player = worldDolls[_key].movementPredicter.gameObject;
                 GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMovement>().target = worldDolls[_key].lumberjack.transform;
             }
         }
